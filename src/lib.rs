b950cef011c87f2ed7490ecf7feb0ddbba2673e1
Yazan Yousefalhaj <yazanyousefalhaj@gmail.com>
2022-04-26 22:12:19 +0300

    minigrep

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_insensitive: bool,
 }
 
 impl Config {
@@ -11,14 +13,61 @@ impl Config {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
-        Ok(Self { query: args[1].clone(), filename: args[2].clone() })
+        let case_insensitive = env::var("CASE_INSENSITIVE").is_ok();
+
+        Ok(Self { query: args[1].clone(), filename: args[2].clone(), case_insensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
+    let results = if config.case_insensitive {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
-    println!("File: {}", contents);
-
+    for line in results {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content.lines().filter(|line| line.contains(&query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    content.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(search(query, content), vec!["safe, fast, productive."]);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+        assert_eq!(search_case_insensitive(query, content), vec!["safe, fast, productive.", "Duct tape"]);
+    }
 }
